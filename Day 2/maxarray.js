a89a17271e843bfc2fbc8ebafe991a310e0ec852
SUSITHRAHARIDAS <68243622+SUSITHRAHARIDAS@users.noreply.github.com>
2020-07-22 19:20:15 +0530

    Add files via upload

---
+++ Day 2/maxarray.js
@@ -0,0 +1,3 @@
+var myarr = [1,2,3,4,5,6];
+var sarr =  myarr.sort(function (a,b){return b-a})
+console.log(sarr[0])
---
+++ Day 2/sumarray.js
@@ -0,0 +1,3 @@
+var myarr = [1,2,3,4,5,6];
+var sum = myarr.reduce((currentval,ele)=>currentval+ele,0) 
+console.log(sum)
